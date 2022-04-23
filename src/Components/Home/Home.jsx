42608a6dc5cfebe61a7088a91ca2922ed0671bd3
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-23 23:26:36 +0600

    Form and list item styled

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,17 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-    const [item, setItem] = useState()
-    useEffect(() => {
-        fetch('http://localhost:5000/item')
-        .then(res => res.json())
-        .then(data => setItem(data))
-    }, [])
-    return (
-        <div>
-            {/* {item.map(food => <li>{food.length}</li>)} */}
-        </div>
-    );
+  const [item, setItem] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/item")
+      .then((res) => res.json())
+      .then((data) => setItem(data));
+  }, []);
+  return (
+    <div>
+      <div className="text-center mt-12">
+        <h3 className="text-4xl font-bold text-emerald-600 mb-8">
+          Food Ordered
+        </h3>
+        {item.map((food) => (
+          <p className="border-2 mb-3 border-gray-500 block w-1/2 mx-auto py-2" key={food._id}>
+            <span className="text-2xl font-semibold">{food.name}</span> - <span className="text-xl font-semibold text-red-600">${food.price}</span><button className="bg-green-600 ml-5 text-white px-2 border-2 border-gray-200">Update</button><button className="bg-red-600 ml-2 text-white px-2 border-2 border-gray-200">X</button>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
